0adf23828e17ec364ff923f132513fdf0695d4d8
Ali1988Baba <alihaydarkutluk@hotmail.com>
2023-03-26 12:05:54 +0300

    Add project files.

---
+++ Second_GitHub_Calculator_CW12/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Second_GitHub_Calculator_CW12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
