0109c1215ff115f4d2a03344d31f01280f4e3838
Ali1988Baba <alihaydarkutluk@hotmail.com>
2023-03-26 12:22:16 +0300

    feat add inputs

--- Second_GitHub_Calculator_CW12/Program.cs
+++ Second_GitHub_Calculator_CW12/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Calculator\n");
+
+            Console.WriteLine("Enter Number 1:");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number 2:");
+            int number2 = int.Parse(Console.ReadLine());
+
+
          
         }
     }
